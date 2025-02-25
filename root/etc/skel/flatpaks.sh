b363a2614f310eb9e84cae707aab9bf1afac1a89
zearp <63272687+zearp@users.noreply.github.com>
2025-02-25 22:38:58 +0100

    hello!

---
+++ root/etc/skel/flatpaks.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+#
+sudo flatpak install \
+	com.github.maoschanz.drawing \
+	com.github.tchx84.Flatseal \
+	com.mattjakeman.ExtensionManager \
+	com.usebottles.bottles \
+	org.gnome.DejaDup \
+	org.gnome.Epiphany \
+	org.gnome.Weather \
+	de.haeckerfelix.Fragments \
+	io.gitlab.librewolf-community
+
